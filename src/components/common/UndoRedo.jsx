ac53a2afddd9f6505f0f99d2d94e259e61fee348
kevin.talmadge@eluv.io <kevin@eluv.io>
2023-08-03 22:02:09 -0700

    Remove hover undo button

--- src/components/common/UndoRedo.jsx
+++ src/components/common/UndoRedo.jsx
@@ -2,21 +2,6 @@ import {Affix, Button, Group, Text} from "@mantine/core";
 import {observer} from "mobx-react-lite";
 import {useLocation} from "react-router-dom";
 import {ArrowNarrowLeft as IconArrowBack, ArrowNarrowRight as IconArrowForward} from "tabler-icons-react";
-import {useHover} from "@mantine/hooks";
-
-const HoverButton = (props) => {
-  const { hovered, ref } = useHover();
-
-  props = {...props};
-  const Render = props.Render;
-  delete props.Render;
-
-  return (
-    <Button {...props} ref={ref}>
-      { Render(hovered) }
-    </Button>
-  );
-};
 
 const UndoRedo = observer(({store, objectId}) => {
   const location = useLocation();
@@ -28,18 +13,6 @@ const UndoRedo = observer(({store, objectId}) => {
   return (
     <Affix position={{bottom: 20, right: 20}}>
       <Group>
-        <HoverButton
-          compact
-          variant="light"
-          disabled={undoActions.length === 0}
-          onClick={() => store.UndoAction({objectId, page})}
-          Render={hovered => (
-            <>
-              <IconArrowBack />
-              { hovered ? <Text>Undo Action</Text> : null }
-            </>
-          )}
-        />
         <Button compact variant="light" disabled={undoActions.length === 0} onClick={() => store.UndoAction({objectId, page})}>
           <IconArrowBack />
           <Text>Undo Action</Text>
